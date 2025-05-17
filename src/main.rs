e3f6ff5c597be24779e3c08f1adc9c3099a3b434
Stattek <stattek1@pm.me>
2025-05-17 18:49:57 -0500

    Cleanup main.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-mod linked_list;
-use linked_list::linked_list::LinkedList;
+mod data_structure;
+use data_structure::linked_list::LinkedList;
 
 fn main() {
-    println!("Hello, world!");
-
     let mut list = LinkedList::<i32>::new();
 
     list.push_front(4);
@@ -11,6 +9,17 @@ fn main() {
 
     println!("First value: {}", list.get(0).unwrap());
 
-    list.remove(0);
+    list.remove(0).unwrap();
     println!("First value: {}", list.get(0).unwrap());
+    list.remove(0).unwrap();
+    // now we have an empty list at this point
+
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    list.push_back(4);
+
+    list.remove(1).unwrap();
+    list.add_at(0, 0).unwrap();
+    list.remove(0).unwrap();
 }
